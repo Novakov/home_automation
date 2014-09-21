46ce7577303ea3448873e9794c00cdf734c99ba8
Maciej Nowak <maciejt.nowak@gmail.com>
2014-09-21 14:40:32 +0200

    Web based led control

--- firmware/src/web/web_handler.erl
+++ firmware/src/web/web_handler.erl
@@ -9,4 +9,12 @@
 handle(Req, 'GET', []) ->
   text("Hello World");
 
+handle(Req, 'GET', ["green", "on"]) ->
+  hw_interface:green_led(on),
+  text("OK");
+
+handle(Req, 'GET', ["green", "off"]) ->
+  hw_interface:green_led(off),
+  text("OK");
+
 handle(_,_,_) -> none.
