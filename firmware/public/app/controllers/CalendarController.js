072c3d19cdf5702c06b6598e949007bdada29ff3
Maciej Nowak <maciejt.nowak@gmail.com>
2014-09-24 19:23:44 +0200

    Selecting events on calendar

--- firmware/public/app/controllers/CalendarController.js
+++ firmware/public/app/controllers/CalendarController.js
@@ -1,9 +1,21 @@
 ﻿'use strict';
 
 angular.module('HomeAutomation.Controllers')
-    .controller('CalendarController', function($scope) {
+    .controller('CalendarController', function ($scope) {
         $scope.eventSources = {
             url: '/events'
+        };
+
+        $scope.addNewEvent = function (start, end) {
+            var event = {
+                title: '',
+                start: start,
+                end:end
+            };
+
+            $scope.calendar.fullCalendar('renderEvent', event, true);
+
+            $scope.calendar.fullCalendar('unselect');
         };
 
         $scope.uiConfig = {
@@ -12,12 +24,16 @@ angular.module('HomeAutomation.Controllers')
                 height: 750,
                 firstDay: 1,
                 allDaySlot: false,
-                editable:true,
+                editable: true,
+                selectable: true,
+                selectHelper:true,
                 columnFormat: {
                     week: 'D.MM dddd'
                 },
                 axisFormat: { 'agenda': 'H:mm' },
-                timeFormat: 'H:mm'
-            }
+                timeFormat: 'H:mm',
+
+                select: $scope.addNewEvent
+            }            
         };
     });
