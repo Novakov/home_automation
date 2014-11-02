9a600c53dfcd0aa487344a749a6a1ba7a5df929b
Maciej Nowak <maciejt.nowak@gmail.com>
2014-11-02 23:00:25 +0100

    Temperature chart

--- firmware/priv/public/app/controllers/TemperatureController.js
+++ firmware/priv/public/app/controllers/TemperatureController.js
@@ -2,11 +2,34 @@
     .controller('TemperatureController', function ($scope, $http, $interval) {
         $scope.currentTemperature = null;
 
+        $scope.temperatures = [
+            {
+                key: "Temperature",
+                values: []
+            }
+        ];
+
+        $scope.xAxisTickFormatFunction = function () {
+            return function (d) {
+                return d3.time.format('%H:%M:%S')(new Date(d));
+            }
+        }
 
         $interval(function () {
             $http.get('/temperature')
                 .then(function (xhr) {
                     $scope.currentTemperature = xhr.data.temperature;
+
+                    var newValues = $scope.temperatures[0].values;
+
+                    newValues.push([new Date().getTime(), xhr.data.temperature]);
+
+                    if (newValues.length > 100) {
+                        newValues.shift();
+                    }
+
+                    $scope.temperatures[0].values = newValues;
+
                 });
         }, 1000);
     });
--- firmware/priv/public/app/controllers/module.js
+++ firmware/priv/public/app/controllers/module.js
@@ -1,2 +1,3 @@
 ﻿'use strict';
-angular.module('HomeAutomation.Controllers', ['ui.calendar', 'ui.bootstrap', 'ui.bootstrap.modal', 'ui.bootstrap.datepicker', 'timepickerPop', 'ui.bootstrap.dropdown']);
+angular.module('HomeAutomation.Controllers', ['nvd3ChartDirectives', 'ui.calendar', 'ui.bootstrap', 'ui.bootstrap.modal', 'ui.bootstrap.datepicker', 'timepickerPop', 'ui.bootstrap.dropdown']);
+angular.module('HomeAutomation.Directives', []);
